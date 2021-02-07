0682cbb857f0a6f9302b75b7be0ac960e7796569
RobinNusantara <robinnusantara127@gmail.com>
2021-02-07 19:03:37 +0700

    validate auth and refactor todo controller

--- src/controllers/todo-controller.ts
+++ src/controllers/todo-controller.ts
@@ -1,120 +1,129 @@
 import {Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client';
 import {getParamsId} from '../utils/utils';
+import Controller from '../utils/controller-interface';
 
 const prisma = new PrismaClient();
 
-async function index(req: Request, res: Response) {
-  try {
-    const todos = await prisma.todo.findMany({orderBy: {createdAt: 'desc'}});
+class TodoController implements Controller {
+  index = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const {id} = req.app.locals.user;
 
-    res.status(200).json({
-      status: 'ok',
-      message: 'Successful fetch todos',
-      results: todos,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'error',
-      message: error.message,
-    });
+      const todos = await prisma.todo.findMany({
+        where: {ownerId: id},
+        orderBy: {createdAt: 'desc'},
+      });
+
+      return res.status(200).json({
+        status: 'ok',
+        message: 'Successful fetch todos',
+        results: todos,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  insert = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const {title, description, timeEnd} = req.body;
+      const date = new Date();
+
+      const todo = await prisma.todo.create({
+        data: {
+          ownerId: 1,
+          title,
+          description,
+          timeStart: date,
+          timeEnd,
+        },
+      });
+
+      return res.status(201).json({
+        status: 'ok',
+        message: 'Successful create todo',
+        results: todo,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  select = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const id: number = getParamsId(req);
+
+      const todo = await prisma.todo.findUnique({where: {id}});
+
+      return res.status(200).json({
+        status: 'ok',
+        message: 'Successful fetch todo',
+        results: todo,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  update = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const id: number = getParamsId(req);
+      const {title, description, isComplete, timeStart, timeEnd} = req.body;
+      const date = new Date();
+
+      const todo = await prisma.todo.update({
+        where: {id},
+        data: {
+          title,
+          description,
+          isComplete,
+          timeStart,
+          timeEnd,
+          updatedAt: date,
+        },
+      });
+
+      return res.status(200).json({
+        status: 'ok',
+        message: 'Successful update todo',
+        results: todo,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  remove = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const id: number = getParamsId(req);
+
+      const todo = await prisma.todo.delete({where: {id}});
+
+      return res.status(200).json({
+        status: 'ok',
+        message: 'Successful remove todo',
+        results: todo,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
   }
 }
 
-async function insert(req: Request, res: Response) {
-  try {
-    const {title, description, timeEnd} = req.body;
-    const date = new Date();
-
-    const todo = await prisma.todo.create({
-      data: {
-        title,
-        description,
-        timeStart: date,
-        timeEnd,
-      },
-    });
-
-    res.status(201).json({
-      status: 'ok',
-      message: 'Successful create todo',
-      results: todo,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-}
-
-async function select(req: Request, res: Response) {
-  try {
-    const id: number = getParamsId(req);
-
-    const todo = await prisma.todo.findUnique({where: {id}});
-
-    res.status(200).json({
-      status: 'ok',
-      message: 'Successful fetch todo',
-      results: todo,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-}
-
-async function update(req: Request, res: Response) {
-  try {
-    const id: number = getParamsId(req);
-    const {title, description, isComplete, timeStart, timeEnd} = req.body;
-    const date = new Date();
-
-    const todo = await prisma.todo.update({
-      where: {id},
-      data: {
-        title,
-        description,
-        isComplete,
-        timeStart,
-        timeEnd,
-        updatedAt: date,
-      },
-    });
-
-    res.status(200).json({
-      status: 'ok',
-      message: 'Successful update todo',
-      results: todo,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-}
-
-async function remove(req: Request, res: Response) {
-  try {
-    const id: number = getParamsId(req);
-
-    const todo = await prisma.todo.delete({where: {id}});
-
-    res.status(200).json({
-      status: 'ok',
-      message: 'Successful remove todo',
-      results: todo,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-}
-
-export {index, insert, select, update, remove};
+export default new TodoController();
--- src/routes/todo-routes.ts
+++ src/routes/todo-routes.ts
@@ -1,15 +1,16 @@
 import BaseRouter from '../utils/router-base';
+import authentication from '../middlewares/auth-middleware';
 import validate from '../middlewares/validate-middleware';
 import TodoSchema from '../validation/todo-validation';
-import * as controller from '../controllers/todo-controller';
+import TodoController from '../controllers/todo-controller';
 
 class TodoRoutes extends BaseRouter {
   routes(): void {
-    this.router.get('/list/all', controller.index);
-    this.router.post('/insert', validate(TodoSchema), controller.insert);
-    this.router.get('/details/:id', controller.select);
-    this.router.patch('/update/:id', controller.update);
-    this.router.delete('/remove/:id', controller.remove);
+    this.router.get('/list/all', authentication, TodoController.index);
+    this.router.post('/insert', validate(TodoSchema), TodoController.insert);
+    this.router.get('/details/:id', authentication, TodoController.select);
+    this.router.patch('/update/:id', authentication, TodoController.update);
+    this.router.delete('/remove/:id', authentication, TodoController.remove);
   }
 }
 
