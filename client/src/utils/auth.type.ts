8b52a6f960b567e151c4c23052826e93f6384975
RobinNusantara <robinnusantara127@gmail.com>
2021-02-09 03:45:31 +0700

    auth interface and type

---
+++ client/src/utils/auth.type.ts
@@ -0,0 +1,19 @@
+export interface AuthState {
+  user: object;
+  isLoading: boolean;
+  error: string;
+};
+
+export type AuthAction = {
+  type: string;
+  payload?: AuthState;
+};
+
+export const AuthActionType = {
+  SIGN_IN_START: 'SIGN_IN_START',
+  SIGN_IN_SUCCESS: 'SIGN_IN_SUCCESS',
+  SIGN_IN_FAILED: 'SIGN_IN_FAILED',
+};
+
+
+export type DispatchType = (args: AuthAction) => AuthAction;
